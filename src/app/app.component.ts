dcc3f6a4acd213420178f9a259420ba1ec295fe0
KoRIOz675 <thomas@mafille.net>
2024-12-06 09:43:22 +0100

    MEDIUM: ci: Creation of the header and navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { AppNavbar } from "./navbar/navbar.component";
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AppNavbar, HeaderComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
