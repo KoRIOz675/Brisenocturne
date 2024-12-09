124a9b00cc906a3351777abb34f3ffe5f7748b08
KoRIOz675 <thomas@mafille.net>
2024-12-09 09:23:02 +0100

    MEDIUM: ci: Continue Header and Navber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
